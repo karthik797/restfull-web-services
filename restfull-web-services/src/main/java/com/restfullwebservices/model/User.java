5d8a25a0d691cf24f5a17689f28c8d6870bc968f
vodafone <vodafone@192.168.1.102>
2019-01-15 11:58:41 +0530

    added swagger definition with custom annotations

--- restfull-web-services/src/main/java/com/restfullwebservices/model/User.java
+++ restfull-web-services/src/main/java/com/restfullwebservices/model/User.java
@@ -5,13 +5,20 @@ import java.util.Date;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel(description="all details about the user")
 public class User {
 
 	private Integer id;
 	
 	@Size(min=2, message="Name should have 2 charectors ")
+	@ApiModelProperty(notes="Name should not have atleast 2 charectors")
 	private String name;
+	
 	@Past
+	@ApiModelProperty(notes="Name should bein the past")
 	private Date dateofbirth;
 	
 
