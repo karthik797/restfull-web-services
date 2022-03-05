9fabfdf745379add084f373c4b6bb906cb484f8d
karthik <karthikr111443@gmail.com>
2022-03-05 12:38:33 +0530

    removed previous spring boot code

--- restfull-web-services/src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-insert into user values(12, sysdate(), 'tet-1'); 
-insert into user values(2, sysdate(), 'tet-1'); 
-insert into user values(3, sysdate(), 'tet-1'); 
-insert into post values(1001, 'my first post', 2);
